0da71fa9359f951a207d808d8bbccea48699b788
Uri Goren <6126173+urigoren@users.noreply.github.com>
2024-03-10 06:54:55 -0400

    one liner

--- emb/run.sh
+++ emb/run.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-
-# Command 1
-command1="python3 db_model.py"
-
-# Command 2
-command2="uvicorn api:app --host 0.0.0.0 --port 8080"
-
-# Run the commands
-sleep 5
-$command1
-sleep 2
-$command2
+sleep 5;python3 db_model.py;sleep 2;uvicorn api:app --host 0.0.0.0 --port 8080
